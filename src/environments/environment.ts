c246d45b44cda18eb8d771699f88a8d3068af717
Quoc An <quocan17102000@gmail.com>
2023-12-29 16:30:42 +0700

    change error code

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 
 export const environment = {
-  PROCESS_SERVICE: 'http://localhost:8080',
-    WS_BASE_URL: 'http://localhost:8080/api/checkin'
+  PROCESS_SERVICE: 'http://localhost:8081',
+    WS_BASE_URL: 'http://localhost:8081/api/banking'
 };
